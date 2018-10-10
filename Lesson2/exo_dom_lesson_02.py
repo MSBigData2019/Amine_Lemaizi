d5ecd02aa3678f193644fab380e3dcc8b1513030
aminelemaizi <contact@lemaizi.com>
2018-10-10 19:40:04 +0200

    Little update : using the 'main'

--- Lesson2/exo_dom_lesson_02.py
+++ Lesson2/exo_dom_lesson_02.py
@@ -60,54 +60,59 @@ Industry : {:,.2f} EUR
 Sector : {:,.2f} EUR
 '''
 
-for key, value in companies.items():
-	# Initialization
-	data = {'company_name': key}
-	page = requests.get(value)
-	soup = BeautifulSoup(page.text, 'html.parser')
+def main():
+	for key, value in companies.items():
+		# Initialization
+		data = {'company_name': key}
+		page = requests.get(value)
+		soup = BeautifulSoup(page.text, 'html.parser')
 
-	# Extract Quarter Sales Dec 2018
-	data_table = soup.find(class_='dataTable')
+		# Extract Quarter Sales Dec 2018
+		data_table = soup.find(class_='dataTable')
 
-	sales = data_table.find_all('tr')[2].find_all('td')
-	data['sales_high'] = float((sales[3].text).replace(',', ''))
-	data['sales_mean'] = float((sales[2].text).replace(',', ''))
-	data['sales_low'] = float((sales[4].text).replace(',', ''))
+		sales = data_table.find_all('tr')[2].find_all('td')
+		data['sales_high'] = float((sales[3].text).replace(',', ''))
+		data['sales_mean'] = float((sales[2].text).replace(',', ''))
+		data['sales_low'] = float((sales[4].text).replace(',', ''))
 
-	# Stock price and evo
+		# Stock price and evo
 
-	stock_info = soup.find(class_="sectionQuoteDetail")
-	data['stock_value'] = float((stock_info.find_all('span')[1].text).strip())
-	stock_evo_text = soup.find(class_="valueContentPercent").text.replace("(", "").replace(")", "").strip()
-	data['stock_evo'] = float(stock_evo_text.replace("%", ""))/100
+		stock_info = soup.find(class_="sectionQuoteDetail")
+		data['stock_value'] = float((stock_info.find_all('span')[1].text).strip())
+		stock_evo_text = soup.find(class_="valueContentPercent").text.replace("(", "").replace(")", "").strip()
+		data['stock_evo'] = float(stock_evo_text.replace("%", ""))/100
 
-	# Percentage of INSTITUTIONAL HOLDERS
-	per_institutional_holders_text = soup.find_all(class_='dataSmall')[-1].find(class_="data").text
-	data['per_institutional_holders'] = float(per_institutional_holders_text.replace("%", ''))/100
+		# Percentage of INSTITUTIONAL HOLDERS
+		per_institutional_holders_text = soup.find_all(class_='dataSmall')[-1].find(class_="data").text
+		data['per_institutional_holders'] = float(per_institutional_holders_text.replace("%", ''))/100
 
-	# Dividend Yield
-	dividend_table = soup.find_all(class_='dataTable')[2]
-	dividends = dividend_table.find_all('tr')[1].find_all('td')
-	data['dividend_sector'] = float((dividends[3].text))
-	data['dividend_industry'] = float((dividends[2].text))
-	data['dividend_company'] = float((dividends[1].text))
+		# Dividend Yield
+		dividend_table = soup.find_all(class_='dataTable')[2]
+		dividends = dividend_table.find_all('tr')[1].find_all('td')
+		data['dividend_sector'] = float((dividends[3].text))
+		data['dividend_industry'] = float((dividends[2].text))
+		data['dividend_company'] = float((dividends[1].text))
 
-	# Collecting data scrapped
-	results.append(data)
+		# Collecting data scrapped
+		results.append(data)
 
-# Printing the results using text canevas
-for result in results:
-	print(
-		text_canevas.format(
-			result['company_name'], 
-			result['sales_high'], 
-			result['sales_mean'],
-			result['sales_low'],
-			result['stock_value'],
-			result['stock_evo'],
-			result['per_institutional_holders'],
-			result['dividend_company'],
-			result['dividend_industry'],
-			result['dividend_sector']
+	# Printing the results using text canevas
+	for result in results:
+		print(
+			text_canevas.format(
+				result['company_name'], 
+				result['sales_high'], 
+				result['sales_mean'],
+				result['sales_low'],
+				result['stock_value'],
+				result['stock_evo'],
+				result['per_institutional_holders'],
+				result['dividend_company'],
+				result['dividend_industry'],
+				result['dividend_sector']
+				)
 			)
-		)
+
+
+if __name__ == "__main__":
+	main()
